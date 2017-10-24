f19d3f7dcfefe37da12c1692ffc68d69baf244f5
xfengcarl <carl.zhang@intel.com>
2017-10-24 20:49:43 +0800

    add perMB skip disable control.
    per MB skip buffer can disable skip/direct for each MB
    Signed-off-by: Carl.Zhang <carl.zhang@intel.com>

--- va/va.h
+++ va/va.h
@@ -1498,6 +1498,13 @@ typedef enum
     VAStatsStatisticsBottomFieldBufferType = 50,
     VAStatsMVBufferType                 = 51,
     VAStatsMVPredictorBufferType        = 52,
+    /** Force MB's to be non skip for encode.it's per-mb control buffer, The width of the MB map
+     * Surface is (width of the Picture in MB unit) * 1 byte, multiple of 64 bytes.
+     * The height is (height of the picture in MB unit). The picture is either
+     * frame or non-interleaved top or bottom field.  If the application provides this
+     *surface, it will override the "skipCheckDisable" setting in VAEncMiscParameterEncQuality.
+     */
+    VAEncMacroblockDisableSkipMapBufferType = 53,
     VABufferTypeMax
 } VABufferType;
 
